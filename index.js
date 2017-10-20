ac04c343770941ab73457035fee0c3c8f461bd9c
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-20 15:57:39 -0700

    figuring out anime

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("bullshit");
+
+
+var path = anime.path('#motionPath path');
+
+var motionPath = anime({
+  targets: '#motionPath .el',
+  translateX: path('x'),
+  translateY: path('y'),
+  rotate: path('angle'),
+  easing: 'linear',
+  duration: 2000,
+  loop: true
+});
+console.log(motionPath);
