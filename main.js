36540b07660f82dad248a8ab47cc9fc58df3b8c9
donkeypagoda <christopherallenstewart@gmail.com>
2017-10-22 10:15:51 -0700

    bringing in tonejs

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var num = 5,
-  rotation = 0,
-  balls = [];
+    rotation = 0,
+    balls = [];
 
 function Ball() {
   this.r = 20;
@@ -18,16 +18,15 @@ function init() {
   for (i = 0; i < num; i++) {
     balls[i] = new Ball();
   }
-  // draw()
-  requestAnimationFrame(draw);
+  draw()
+  // requestAnimationFrame(draw);
 };
 
 function draw() {
   // reset transforms before clearing
   context.setTransform(1, 0, 0, 1, 0, 0);
   context.clearRect(0, 0, canvas.width, canvas.height);
-  // console.log(context);
-  // tramslate and rotate an absolute rotation value
+  // translate and rotate an absolute rotation value
   context.translate(300, 300);
   context.rotate(rotation);
 
@@ -40,8 +39,8 @@ function draw() {
   //   context.fill();
   // }
   let ball = new Ball();
-  ball.x = 170
-  ball.y = 170
+  ball.x = 240
+  ball.y = 0
   context.beginPath();
   context.arc(ball.x, ball.y, ball.r, 0, 2 * Math.PI, false);
   context.stroke();
@@ -49,12 +48,15 @@ function draw() {
 
   context.beginPath();
   context.arc(0, 0, 240, 0, Math.PI * 2, false);
-  console.log(context);
+  // console.log(ball.x);
+  // if()
+  console.log(rotation.toFixed(2) * 100)
+  console.log(36000 * (Math.PI / 180));
   context.lineWidth = 8;
   context.stroke();
 
 
   // update rotation value and request new frame
-  rotation -= 0.01;
-  requestAnimationFrame(draw)
+  rotation -= 0.0001;
+  // requestAnimationFrame(draw)
 }
